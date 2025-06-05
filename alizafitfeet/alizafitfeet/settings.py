e98f1d873b9c87a7459073bb3f3577db070e56b4
gentwocoder <adetoyese0511@gmail.com>
2025-06-05 22:33:57 +0100

    updated settings

--- alizafitfeet/alizafitfeet/settings.py
+++ alizafitfeet/alizafitfeet/settings.py
@@ -1,4 +1,5 @@
 import os
+# import dj_database_url
 from decouple import config
 from pathlib import Path
 
@@ -71,6 +72,18 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config('DB_NAME'),
+#         'USER': config('DB_USER'),
+#         'PASSWORD': config('DB_PASSWORD'),
+#         'HOST': config('DB_HOST'),
+#         'PORT': config('DB_PORT'),
+#     }
+# }
+
 
 
 # Password validation
