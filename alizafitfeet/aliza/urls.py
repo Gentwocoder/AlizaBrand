4453fa03abca88595f8c0448c4ac9289aad6f9d3
gentwocoder <adetoyese0511@gmail.com>
2025-07-29 10:14:51 +0100

    comment out media URL and root settings in settings.py; disable media serving in urls.py

--- alizafitfeet/aliza/urls.py
+++ alizafitfeet/aliza/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('collections/', views.collections, name='collections'),
     path('contact/', views.contact, name='contact'),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
